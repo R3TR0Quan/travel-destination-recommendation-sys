a539138111cd076ae15f979d6628dc928a3b1edf
imacharia <macharia.gichoya@gmail.com>
2023-07-13 18:42:00 +0300

    reviews analysis and modelling

--- deployment/africura_2_1.py
+++ deployment/africura_2_1.py
@@ -130,6 +130,7 @@ clean_df, tfidfv_matrix2, cosine_sim2, cosine_similarities, indices = load_data(
 
 # Initialize the RecommenderSystem object
 recommender = RecommenderSystem(clean_df, tfidfv_matrix2, cosine_sim2, cosine_similarities, indices)
+
 def main():
     st.title("Recommender System")
 
@@ -167,115 +168,6 @@ def main():
 
     # Render the sidebar
     st.sidebar.render()
-    # Set the CSS style
-    css = '''
-    <style>
-        /* Global styles */
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-image: url('../Data/images/ui_bg.jpg'); /* Updated path to background image */
-            background-size: cover;
-            background-position: center;
-        }
-        /* Header styles */
-        header {
-            background-color: white;
-            color: #fff;
-            padding: 20px;
-            text-align: center;
-        }
-        h1 {
-            margin: 0;
-        }
-        /* Navigation styles */
-        nav {
-            background-color: #f2f2f2;
-            padding: 10px;
-            text-align: center; 
-        }
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-        }
-        li {
-            display: inline;
-            margin-right: 10px;
-        }
-        a {
-            color: #333;
-            text-decoration: none;
-            padding: 5px;
-        }
-        /* Section styles */
-        section {
-            padding: 20px;
-            margin-bottom: 20px;
-            background-color: #f9f9f9;
-            opacity: 0.9;
-            border-radius: 10px;
-        }
-        h2 {
-            margin-top: 0;
-        }
-        /* Search section styles */
-        #search-section {
-            background-image: url('../Data/images/ui_bg.jpg');
-            background-size: cover;
-            background-position: center;
-            padding: 20px;
-            border-radius: 10px;
-            text-align: center;
-            margin: 20px auto;
-            width: 80%;
-            max-width: 600px;
-        }
-        input[type="number"] { /* Updated input type to number */
-            padding: 10px;
-            width: 100%;
-            margin-bottom: 10px;
-            border-radius: 5px;
-            border: 1px solid #ccc;
-        }
-        input[type="submit"] {
-            padding: 10px 20px;
-            background-color: #333;
-            color: #fff;
-            border: none;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-        /* Footer styles */
-        footer {
-            background-color: #333;
-            color: #fff;
-            padding: 10px;
-            text-align: center;
-        }
-    </style>
-    '''
 
-    # Render the HTML code
-    st.markdown(css, unsafe_allow_html=True)
-    st.markdown("<header><h1>Africura Travel Destination Recommendation System</h1></header>", unsafe_allow_html=True)
-
-
-    
-    with st.markdown("## Contact"):
-        with st.form(key='contact-form'):
-            st.markdown("Any queries? Please fill out the form below and we will get back to you as soon as possible.")
-            st.markdown("### Message")
-            message = st.text_area(label='Enter your message here')
-            st.markdown("### Contact Information")
-            name = st.text_input(label='Name')
-            email = st.text_input(label='Email')
-            phone = st.text_input(label='Phone')
-            st.markdown("###")
-            submit_button = st.form_submit_button(label='Submit')
-            if submit_button:
-                st.markdown("Thank you for getting in touch. We will get back to you as soon as possible.")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
