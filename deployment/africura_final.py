ca44f614fc09969ef108d75b0a4cf400b8bd0d4c
imacharia <macharia.gichoya@gmail.com>
2023-08-29 10:58:27 +0300

    removing large files from repo

--- deployment/africura_final.py
+++ deployment/africura_final.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 import glob
 import re
+import os
 import pickle
 import ast
 import matplotlib.pyplot as plt
@@ -23,26 +24,92 @@ from PIL import Image
 @st.cache_resource
 def load_data():
     # Load the clean_df DataFrame
-    with open(r'../data/clean_df.pkl', 'rb') as f:
+    with open(r'Data/clean_df.pkl', 'rb') as f:
         clean_df = pickle.load(f)
 
     # Load the pickled files
-    with open(r'../data/tfidf_matrix2.pkl', 'rb') as f:
+    with open(r'Data/tfidf_matrix2.pkl', 'rb') as f:
         tfidfv_matrix2 = pickle.load(f)
 
-    with open(r'../data/.cosine_sim2.pkl', 'rb') as f:
+    with open(r'Data/cosine_sim2.pkl', 'rb') as f:
         cosine_sim2 = pickle.load(f)
 
-    with open(r'../data/.cosine_similarities.pkl', 'rb') as f:
+    with open(r'Data/cosine_similarities.pkl', 'rb') as f:
         cosine_similarities = pickle.load(f)
 
-    with open(r'../data/.indices.pkl', 'rb') as f:
+    with open(r'Data/.indices.pkl', 'rb') as f:
         indices = pickle.load(f)
 
     return clean_df, tfidfv_matrix2, cosine_sim2, cosine_similarities, indices
 
 st.set_page_config(layout="wide")
-    
+
+
+
+    # Add background image and custom styles
+page_bg_img_about = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://cdn.pixabay.com/photo/2019/09/20/23/47/sand-4492751_640.jpg");
+    background-size: cover;
+    background-position: top left;
+    background-repeat: no-repeat;
+}
+
+[data-testid="stHeader"] {
+    background-color: rgba(0,0,0,0);
+}
+
+[data-testid="Toolbar"] {
+    right: 2rem;
+}
+</style>
+"""
+st.markdown(page_bg_img_about, unsafe_allow_html=True)
+
+
+page_bg_img_recomender = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://cdn.pixabay.com/photo/2016/10/20/22/18/solitude-1756736_640.jpg");
+    background-size: cover;
+    background-position: top left;
+    background-repeat: no-repeat;
+}
+
+[data-testid="stHeader"] {
+    background-color: rgba(0,0,0,0);
+}
+
+[data-testid="Toolbar"] {
+    right: 2rem;
+}
+</style>
+"""
+st.markdown(page_bg_img_recomender, unsafe_allow_html=True)
+
+
+page_bg_img_gallery = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://cdn.pixabay.com/photo/2018/10/14/20/38/sunset-3747442_640.jpg");
+    background-size: cover;
+    background-position: top left;
+    background-repeat: no-repeat;
+}
+
+[data-testid="stHeader"] {
+    background-color: rgba(0,0,0,0);
+}
+
+[data-testid="Toolbar"] {
+    right: 2rem;
+}
+</style>
+"""
+st.markdown(page_bg_img_gallery, unsafe_allow_html=True)
+
+
 
 class RecommendationEngine:
     def __init__(self, clean_df, cosine_sim2, cosine_similarities):
@@ -59,7 +126,7 @@ class RecommendationEngine:
         indices = [x for x, _ in sim_scores]
         return self.clean_df.set_index('name').iloc[indices][
             ['country', 'RankingType', 'subcategories', 'LowerPrice', 'UpperPrice']
-        ]
+        ].astype({'LowerPrice': int, 'UpperPrice': int})
 
     def select_amenities(self, combined_amenities, cosine_similarities, cosine_sim2):
         indices = {title: index for index, title in enumerate(self.clean_df['combined_amenities'])}
@@ -76,16 +143,23 @@ class RecommendationEngine:
             return f"No recommendations found for {combined_amenities}."
 
 
-    def recommend_amenities(self, amenities, cosine_similarities, cosine_sim2):
-        indices = {title: index for index, title in enumerate(self.clean_df['amenities'])}
-        idx = indices[amenities]
+    def recommend_amenities(self, combined_amenities):
+        indices = {title: index for index, title in enumerate(self.clean_df['combined_amenities'])}
+        idx = indices[combined_amenities]
         sim_scores = list(enumerate(np.dot(self.cosine_sim2[idx], self.cosine_similarities)))
         sim_scores.sort(key=lambda x: x[1], reverse=True)
         sim_scores = sim_scores[1:11]
         indices = [x for x, _ in sim_scores]
-        return self.clean_df.set_index('amenities').iloc[indices][
-            ['name', 'country', 'RankingType', 'subcategories', 'LowerPrice', 'UpperPrice']
-        ]
+        return self.clean_df.set_index('combined_amenities').iloc[indices][
+            [
+                'name',
+                'country',
+                'RankingType',
+                'subcategories',
+                'LowerPrice',
+                'UpperPrice',
+            ]
+        ].astype({'LowerPrice': int, 'UpperPrice': int})
         
     
     def recommend_subcategory(self, subcategories, cosine_similarities, cosine_sim2):
@@ -97,12 +171,12 @@ class RecommendationEngine:
         indices = [x for x, _ in sim_scores]
         return self.clean_df.set_index('subcategories').iloc[indices][
             ['name', 'country', 'RankingType', 'type', 'LowerPrice', 'UpperPrice']
-        ]    
+        ].astype({'LowerPrice': int, 'UpperPrice': int})    
 
     def recommend_attraction(self, rating_threshold, cosine_similarities, cosine_sim2):
         recommendations = self.clean_df[self.clean_df['rating'] < rating_threshold][
             ['name', 'LowerPrice', 'UpperPrice', 'amenities', 'type', 'country']
-        ]
+        ].astype({'LowerPrice': int, 'UpperPrice': int})
         recommendations.reset_index(drop=True, inplace=True)
         return recommendations
 
@@ -115,10 +189,12 @@ class RecommendationEngine:
         indices = [x for x, _ in sim_scores]
         recommended_country = self.clean_df.set_index('country').iloc[indices][
             ['name', 'city', 'RankingType', 'subcategories', 'LowerPrice', 'UpperPrice']
-        ]
+        ].astype({'LowerPrice': int, 'UpperPrice': int})
 
         filtered_recommendations = recommended_country[recommended_country.index == country]
         return pd.DataFrame(filtered_recommendations)
+
+
 def create_account():
     """Creates a new account."""
 
@@ -149,27 +225,32 @@ def main():
        # _extracted_from_main_89()  
         
     # Set the CSS style
-    st.markdown(
-        """<style>
-            body {
-                background-image: Image.open("../Data/images/ui_bg.jpg");
-                background-size: cover;
-                
-            }
-        </style>"""
-    , unsafe_allow_html=True)
+    #st.markdown(
+     #   """<style>
+          #  body {
+          #      background-image: Image.open("../Data/images/ui_bg.jpg");
+          #      background-size: cover;            
+          #  }
+        #</style>"""
+    #, unsafe_allow_html=True)
     
 
 
 
    # Add other sections using st.markdown()
+    st.sidebar.header("Africura!")
+    
+                
     st.sidebar.subheader("About")
     st.sidebar.write("Africura is a recommendation engine that provides suggestions for locations to visit in Africa based on given preferences")
 
                 
     if selection == "Join":
+        # Add header
+        st.markdown("<h1 style='color: rgba(3, 3, 3, 6)'>Africura Members</h1>", unsafe_allow_html=True)
+        st.write("<h6 style='color: rgba(3, 3, 3, 6)'> Coming Soon! Watch this space.</h6>", unsafe_allow_html=True)
         create_account()
-            
+          
     
     # Sidebar
     #option = st.sidebar.radio("Select Recommendation Type", ["Attraction", "Place", "Preferences", "Amenities"])
@@ -178,14 +259,17 @@ def main():
     if selection == "About" :
         with st.sidebar.expander(""):
             pass
-        st.markdown("# Welcome to Africura!")
-        st.markdown("##### A one stop shop for all you WanderLusters. ")
-        st.write("## Africa Travel Recommendation System")
+        
+        # Add header
+        st.markdown("<h1 style='color: rgba(3, 3, 3, 6)'>Welcome to Africura</h1>", unsafe_allow_html=True)
+        
+        st.markdown("##### A one-stop shop for all you WanderLusters. ")
+        #st.write("## Africa Travel Recommendation System")
         
         
-        st.write("This recommendation system uses machine learning to recommend places to visit in Africa. The system takes into account a user's interests, budget, and travel dates to generate personalized recommendations.")
-        st.write("Top Destinations in Africa: Analyze the data from your system to identify the top tourist destinations in Africa based on customer ratings, popularity, and positive reviews. Highlight these destinations to attract users and showcase the most sought-after locations.") 
-        st.write("Customer Loyalty and Engagement: Leverage your recommendation system to foster customer loyalty and encourage repeat customers. Provide incentives or rewards for users who book multiple trips or engage with your platform frequently. Offer personalized promotions or discounts for their preferred destinations to enhance customer engagement and satisfaction. Continuous Improvement: Collect user information and feedback to improve the recommendations in the long run. Implement mechanisms to gather user reviews and ratings for destinations they have visited through your system. Utilize this feedback to refine your recommendation algorithms, enhance the accuracy of predictions, and provide even better suggestions to future users.") 
+        st.write("This recommendation system uses machine learning to recommend places to visit in Africa. The system takes into account a user's interests, and budget to generate recommendations.")
+        # st.write("Top Destinations in Africa: Analyze the data from your system to identify the top tourist destinations in Africa based on customer ratings, popularity, and positive reviews. Highlight these destinations to attract users and showcase the most sought-after locations.") 
+        # st.write("Customer Loyalty and Engagement: Leverage your recommendation system to foster customer loyalty and encourage repeat customers. Provide incentives or rewards for users who book multiple trips or engage with your platform frequently. Offer personalized promotions or discounts for their preferred destinations to enhance customer engagement and satisfaction. Continuous Improvement: Collect user information and feedback to improve the recommendations in the long run. Implement mechanisms to gather user reviews and ratings for destinations they have visited through your system. Utilize this feedback to refine your recommendation algorithms, enhance the accuracy of predictions, and provide even better suggestions to future users.") 
         map_data = {
                     # Define the map layout
                     'layout': go.Layout(
@@ -212,7 +296,7 @@ def main():
                         opacity=0.8
                         ),
                     text=['Price: ${}'.format(i) for i in clean_df['UpperPrice']],
-                    hovertext=clean_df.apply(lambda x: f"Ranking Type: ${x['RankingType']}, Location: {x['locationString']}", axis=1),
+                    hovertext=clean_df.apply(lambda x: f"Ranking Type: ${x['RankingType']}, Location: {x['Location']}", axis=1),
                         )
                 }
 
@@ -241,24 +325,14 @@ def main():
 #def _extracted_from_main_89():    
     if selection == "Recomenders" : 
         with st.sidebar.expander(""):
-            pass    
-        st.title("Recommender System")
-    
-        # Set the CSS style
-        st.markdown("""
-            <style>
-                .st-sidebar {
-                    width: 100px;
-                }
-            </style>
-        """, unsafe_allow_html=True)
-
-        # Get the user's preferences
-        #preferences = st.multiselect("What are your preferences?", ["Hotel", "Restaurant", "Culture", "Specialty Lodging", "Bed and Breakfast","Pool", "Adventure"])
-
+            pass
+        # Add header
+        st.markdown("<h1 style='color: rgba(3, 3, 3, 6)'>Africura Recomender</h1>", unsafe_allow_html=True)
+        
 
         # Recommend locations based on the user's preferences
         st.header("Attraction Recommendation")
+        st.write("Disclaimer: Prices are only an estimate")
         rating_threshold = st.number_input("Enter Rating Threshold", min_value=0.0, max_value=5.0, value=3.0, step=0.1)
 
         if st.button("Get Attraction Recommendations"):
@@ -288,7 +362,7 @@ def main():
             st.write(recommended_subcategory)
 
        
-        amenities = clean_df['amenities'].str.split(', ').explode().unique()
+        amenities = clean_df['combined_amenities'].str.split(', ').explode().unique()
 
         if len(amenities) >= 0:
             # Convert the amenities array to a list
@@ -298,7 +372,7 @@ def main():
                 "Select Amenities", amenities_list
             ):
                 # Filter the clean_df DataFrame based on the selected amenities
-                filtered_df = clean_df[clean_df['amenities'].str.contains('|'.join(selected_amenities))]
+                filtered_df = clean_df[clean_df['combined_amenities'].str.contains('|'.join(selected_amenities))]
 
                 # Get the recommendations for the selected amenities
                 recommendations = recommender.select_amenities(selected_amenities[0], cosine_sim2, cosine_similarities)
@@ -312,39 +386,81 @@ def main():
     if selection == "Gallery" :
         with st.sidebar.expander(""):
             pass
-        
-        def gallery(gallery_images):
-            """Displays a gallery of images."""
-            if len(gallery_images) > 0:
-                st.markdown("### Gallery Images")
-                num_images_per_row = 3
-                columns = st.columns(num_images_per_row)
-              
-                for i, image in enumerate(gallery_images):
-                    row_columns = columns[i % num_images_per_row]
+        # Add header
+        st.markdown("<h1 style='color: rgba(3, 3, 3, 6)'>Africura Gallery</h1>", unsafe_allow_html=True)
+        # List of image URLs from Pixabay
+        image_urls = [
+                        "https://cdn.pixabay.com/photo/2018/03/17/10/05/wildlife-3233525_640.jpg",
+                        "https://cdn.pixabay.com/photo/2018/01/21/18/54/water-buffalo-3097317_640.jpg",
+                        "https://cdn.pixabay.com/photo/2018/03/03/10/02/architecture-3195322_640.jpg",
+                        "https://cdn.pixabay.com/photo/2017/07/14/04/50/cheetah-2502782_640.jpg",
+                        "https://cdn.pixabay.com/photo/2018/09/17/14/36/lion-3683994_640.jpg",
+                        "https://cdn.pixabay.com/photo/2014/06/20/18/34/ostriches-373339_640.jpg",
+                        "https://cdn.pixabay.com/photo/2016/02/24/06/35/cape-of-good-hope-1219192_640.jpg",
+                        "https://cdn.pixabay.com/photo/2017/05/10/23/10/morocco-2302244_640.jpg",
+                        # Add more image URLs from Pixabay
+                        ]
 
-                    image_name = image.split("/")[-1].split(".")[0]
-                    row_columns.image(image, caption=image_name, width=400)
+        # Display the images in the gallery
+        for image_url in image_urls:
+            image_name = os.path.basename(image_url)  # Extract the name from the URL
+            st.image(image_url, caption=image_name, use_column_width=True)
+            
+            
+            
+            
+            
+        def gallery(gallery_files):
+            """Displays a gallery of images and videos."""
+            if len(gallery_files) > 0:
+                st.markdown("### Gallery")
+                num_files_per_row = 3
+                columns = st.columns(num_files_per_row)
+
+                for i, file_path in enumerate(gallery_files):
+                    row_columns = columns[i % num_files_per_row]
+                    file_extension = file_path.split(".")[-1]
+
+                    if file_extension in ["png", "jpg", "jpeg"]:
+                        file_type = "image"
+                        file_caption = file_path.split("/")[-1].split(".")[0]
+                        row_columns.image(file_path, caption=file_caption, width=400)
+                    elif file_extension == "mp4":
+                        file_type = "video"
+                        file_caption = file_path.split("/")[-1].split(".")[0]
+                        row_columns.markdown(f"**{file_caption}**")
+                        row_columns.video(file_path)
+                    else:
+                        continue
+
             else:
-                st.markdown("No images found in the gallery.")
+                st.markdown("No files found in the gallery.")
 
-        gallery_images = glob.glob("../Data/images/gallery/*.jpeg")
-        gallery(gallery_images)
+        gallery_files = glob.glob("Data/images/gallery/*.*")  # Update the file path accordingly
+        gallery(gallery_files)
 
         with st.form(key='gallery-form'):
-            image_name = st.text_input("Image Name")
+            file_name = st.text_input("File Name")
             st.markdown("### Message")
             message = st.text_area(label='Enter your message here')
-            image_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
-            if st.form_submit_button("Add Image"):
-                if image_file is not None:
-                    image_path = f"../Data/images/gallery/{image_name}.jpeg"
-                    with open(image_path, "wb") as f:
-                        f.write(image_file.read())
-                gallery_images.append(image_path)
-                st.success("Image added successfully!")
+            file = st.file_uploader("Upload File", type=["png", "jpg", "jpeg", "mp4"])
+
+            if st.form_submit_button("Add File"):
+                if file is not None:
+                    file_extension = file.name.split(".")[-1]
+                    if file_extension == "mp4":
+                        file_type = "video"
+                else:
+                    file_type = "image"
+
+                file_path = f"Data/images/gallery/{file_name}.{file_extension}"
+                with open(file_path, "wb") as f:
+                    f.write(file.read())
+
+                gallery_files.append(file_path)
+                st.success(f"{file_type.capitalize()} added successfully!")
             else:
-                st.error("Please upload an image.")
+                st.error("Please upload a file.")
                 
 if __name__ == "__main__":
     main()
